0a08eeec2b864ca24c9a0c038daa5e9395f6924e
ViniciusGavaPereira <74336299+ViniciusGavaPereira@users.noreply.github.com>
2021-01-03 22:42:38 -0300

    Add files via upload

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,41 +1,17 @@
-
-/* $(".question").click(function(){
-
-    var x = "none"
-
-
-    if(x == "none"){
-
-        $(this).children(".answer").toggle()
-
-        x = "visible"
-
-    }else{
-
-        $(".answer").hide()
-
-
-        $(this).children(".answer").toggle()
-
-        
-    }
-
-
-})
-
- */
-
-
-
 $(".question").click(function(){
-    var display = $(this).children(".answer").css('display')
+
+    var display = $(this).children(".answer").css('display');
+
+    $(this).children('img').css('transform', 'rotate(180deg)');
     
     $(".answer").hide()
-    
+
     $(this).children(".answer").toggle()
-  
+
     if(display == 'block') {
-        $(this).children(".answer").hide()
+        $(this).children(".answer").hide();
+        $(this).children('img').css('transform', "rotate(0deg)");
+
     }
     
 })
